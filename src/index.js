3d2ef27c2f94b0d2035fb05e580af5c9cfa5bbb5
sundeeep <sandeepkumar25820@gmail.com>
2024-06-14 12:56:58 +0530

    [configuration] aws, youtube and db config added.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
+require("dotenv").config();
 const express = require("express");
-const axios = require("axios");
-const qs = require("qs");
 const fs = require("fs");
 const https = require("https");
 const multer = require("multer");
 const path = require("path");
 const http = require("http");
 const cookieParser = require("cookie-parser");
-const cors = require('cors');
-const compression = require('compression');
+const cors = require("cors");
+const compression = require("compression");
 
+const connectDB = require("./config/mongodb.config");
+const authRoutes = require("./routes/authRoutes");
+const workspaceRoutes = require("./routes/workspaceRoutes");
+const youtubeRoutes = require("./routes/youtubeRoutes");
+const sessionRoutes = require("./routes/sessionRoutes");
+const userRoutes = require("./routes/userRoutes");
 
+const errorHandler = require("./middlewares/errorHandler");
+const sessionMiddleware = require("./middlewares/session"); // Ensure correct import
+const logoutIfExpired = require("./middlewares/logoutIfExpired");
 
 const app = express();
+
+connectDB();
 
 const allowedOrigins = [
   "http://localhost:3000", // Development frontend URL
   "https://yteditor.vercel.app", // Production frontend URL
 ];
-
 
 // CORS options
 const corsOptions = {
@@ -26,13 +35,17 @@ const corsOptions = {
     if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
+const upload = multer({ dest: "uploads/" });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Use compression middleware
 app.use(compression());
@@ -42,21 +55,16 @@ app.use(cookieParser());
 
 // Your other middleware and routes
 app.use(express.json());
+app.use(sessionMiddleware); // Use session middleware directly
+// app.use(logoutIfExpired);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/workspaces", workspaceRoutes);
+app.use("/api/youtube", youtubeRoutes);
+app.use("/api/session", sessionRoutes);
+app.use("/api/users", userRoutes);
+app.use(errorHandler);
 
-app.get('/getName', (req, res) => {
-  res.status(200).json({ name: "Sandeep" });
-})
-
-
-const upload = multer({ dest: "uploads/" });
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 
 const httpsOptions = {
@@ -64,7 +72,8 @@ const httpsOptions = {
   cert: fs.readFileSync("server.crt"),
 };
 
+app.get("/", (req, res) => res.send("Hello"));
+
 const server = http.createServer(app);
-server.listen(3699, () => {
-  console.log("Server started on port 3699");
-});
+const PORT = process.env.PORT || 3699;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
